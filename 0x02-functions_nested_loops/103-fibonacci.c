7c83f85e61e83fd3eacfe2591beccbcdc199efb8
Andrew <49104171+carsonak@users.noreply.github.com>
2023-09-13 21:41:03 +0300

    Added 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ *fibonacci - prints out the first 50 fibonacci numbers
+ *
+ *Return: void
+ */
+
+void fibonacci(void)
+{
+	long long int a, b, c, sum;
+
+    c = 2;
+	a = 1;
+    sum = 0;
+	printf("1, 2, ");
+
+	while (c <= 4,000,000)
+	{
+		b = c;
+        c += a;
+		a = b;
+
+		if (c % 2 == 0)
+        {
+            sum += c;
+        }
+	}
+    printf("%d", sum);
+}
+
+/**
+ *main - calls fibonacci
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	fibonacci();
+	return (0);
+}
