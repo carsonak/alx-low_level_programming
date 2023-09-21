ece3c7a31ff602013a4cf7e0c8402f6212d41a7b
Andrew <49104171+carsonak@users.noreply.github.com>
2023-09-21 10:11:25 +0300

    Added notes to 101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  *print_number - prints the given number
  *@n: an integer
@@ -11,6 +10,7 @@ void print_number(int n)
 {
 	unsigned int num, div = 1;
 
+	/*Turning -ve numbers to +ve*/
 	if (n < 0)
 	{
 		num = -n;
@@ -19,10 +19,12 @@ void print_number(int n)
 	else
 		num = n;
 
+	/*Calculating the highest total value of num*/
 	while (num / div > 9)
 	{
 		div *= 10;
 	}
+	/*Printing out the number from left to right*/
 	while (div > 0)
 	{
 		_putchar('0' + (num / div));
