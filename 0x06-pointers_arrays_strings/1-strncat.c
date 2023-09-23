bcf2c2815a586d0c94b18b86d43143a9f377d6f6
Andrew <49104171+carsonak@users.noreply.github.com>
2023-09-23 17:32:51 +0300

    Corrections for 2

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -18,17 +18,14 @@ char *_strncat(char *dest, char *src, int n)
 		i++;
 	}
 
-	if (dest[i] != '\0' && src[i] != '\0')
+	j = 0;
+	while (j < n && src[j] != '\0')
 	{
-		j = 0;
-		while (j < n)
-		{
-			dest[i] = src[j];
-			i++;
-			j++;
-		}
-		dest[i] = '\0';
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
+	dest[i] = '\0';
 
 	return (dest);
 }
