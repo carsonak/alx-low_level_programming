8dd7d5ce7229dd10005906c1576063eafacb20a3
Andrew <49104171+carsonak@users.noreply.github.com>
2023-09-25 20:00:56 +0300

    Added more headers

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,28 +1,19 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-#include <unistd.h>
-
-#include <stdio.h>
-
 #include <string.h>
-
+#include <unistd.h>
+#include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <stdbool.h>
 int _putchar(char c);
-
 void simple_print_buffer(char *buffer, unsigned int size);
-
 char *_memset(char *s, char b, unsigned int n);
-
 char *_memcpy(char *dest, char *src, unsigned int n);
-
 char *_strchr(char *s, char c);
-
 unsigned int _strspn(char *s, char *accept);
-
 char *_strpbrk(char *s, char *accept);
-
 char *_strstr(char *haystack, char *needle);
-
-
 
 #endif
