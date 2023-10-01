641cb3a6e724ed15fa586898e6f11988960442cb
Andrew <49104171+carsonak@users.noreply.github.com>
2023-10-01 19:58:54 +0300

    Changed the strlen funct

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,14 +4,29 @@
  *print_rev - prints out a string in reverse
  *@s: the string
  */
-
 void print_rev(char *s)
 {
-	int a, i;
+	int i;
 
-	a = strlen(s);
-
-	for (i = a - 1; i >= 0; i--)
+	for (i = _strlen(s) - 1; i >= 0; i--)
 		_putchar(*(s + i));
+
 	_putchar('\n');
+}
+
+/**
+ *_strlen - prints string length.
+ *@s: string
+ *
+ *Return: lenth of the string
+ */
+
+int _strlen(char *s)
+{
+	int i, cnt = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		cnt++;
+
+	return (cnt);
 }
