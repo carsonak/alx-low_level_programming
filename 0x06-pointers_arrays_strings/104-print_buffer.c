977bfb20d9f1b460fba1b8bbae760c11a8991680
Andrew <49104171+carsonak@users.noreply.github.com>
2023-10-16 04:04:46 +0300

    Added main.h and main.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,8 +1,38 @@
 #include "main.h"
 
-/***/
+/**
+ * print_buffer - prints the buffer in an array
+ * @b: The array
+ * @size: SIze of type
+ */
 void print_buffer(char *b, int size)
 {
+	int idx = 0, idy = 0;
 
-	printf("%x", );
+	while (b[idx] != '\0')
+	{
+		idx++;
+	}
+
+	idx++;
+
+	printf("%08x: ", idy);
+	while (idx < size)
+	{
+		if (idx % 2)
+			printf("%02x%02x ", b[idx], b[idx + 1]);
+
+		if (!(idx % 10))
+		{
+			for (idy = idx - 10; idy <= idx; idy++)
+				if (b[idy] >= ' ' && b[idy] <= '~')
+					printf("%c", b[idy]);
+				else
+					putchar('.');
+
+			printf("\n%08x: ", idy);
+		}
+		idx++;
+	}
+	putchar('\n');
 }
