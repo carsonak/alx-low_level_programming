8e121ef05fe3e29ac7bc969ab983c94cda41f1e2
Andrew <49104171+carsonak@users.noreply.github.com>
2023-10-20 05:48:51 +0300

    Used printf

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,96 +8,24 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t i, count = 1;
-	char *wrd, *null = "[0] (nil)";
+	size_t count = 1;
 	const list_t *ptr = h;
 
 	while (ptr->next)
 	{
 		if (ptr->str)
-		{
-			wrd = str_fil(ptr->str, ptr->len);
-			for (i = 0; wrd[i]; i++)
-				_putchar(wrd[i]);
-
-			free(wrd);
-		}
+			printf("[%d] %s\n", ptr->len, ptr->str);
 		else
-			for (i = 0; null[i]; i++)
-				_putchar(null[i]);
+			printf("[0] (nil)\n");
 
 		count++;
 		ptr = ptr->next;
 	}
 
 	if (ptr->str)
-	{
-		wrd = str_fil(ptr->str, ptr->len);
-		for (i = 0; wrd[i]; i++)
-			_putchar(wrd[i]);
-
-		free(wrd);
-	}
+		printf("[%d] %s\n", ptr->len, ptr->str);
 	else
-		for (i = 0; null[i]; i++)
-			_putchar(null[i]);
+		printf("[0] (nil)\n");
 
 	return (count);
-}
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
- * str_fil - Formats the string of a linked list
- * @str: String
- * @len: Length of the string
- *
- * Return: pointer to new String
- */
-char *str_fil(char *str, int len)
-{
-	int i, sLen = len;
-	char *nStr;
-
-	if (len > 9 && len < 100)
-	{
-		sLen += 7;
-		nStr = malloc(sizeof(*nStr) * sLen);
-		nStr[0] = '[';
-		nStr[1] = (len / 10) + '0';
-		nStr[2] = (len % 10) + '0';
-		nStr[3] = ']';
-		nStr[4] = ' ';
-		for (i = 0, len = 5; str[i]; len++, i++)
-			nStr[len] = str[i];
-
-		nStr[len] = '\n';
-		nStr[len + 1] = '\0';
-	}
-	else
-	{
-		sLen += 6;
-		nStr = malloc(sizeof(*nStr) * sLen);
-		nStr[0] = '[';
-		nStr[1] = len + '0';
-		nStr[2] = ']';
-		nStr[3] = ' ';
-		for (i = 0, len = 4; str[i]; len++, i++)
-			nStr[len] = str[i];
-
-		nStr[len] = '\n';
-		nStr[len + 1] = '\0';
-	}
-
-	return (nStr);
 }
