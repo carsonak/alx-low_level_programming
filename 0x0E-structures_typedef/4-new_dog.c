250dd7bc0f4871d98a27f5556112e173c7b854d8
Andrew <49104171+carsonak@users.noreply.github.com>
2023-10-25 19:57:56 +0300

    More adjustments

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,16 +10,25 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	char *nm = name;
-	char *ownr = ownr;
+	char *name_cpy = malloc(sizeof(*name_cpy) * (strlen(name) + 1));
+	char *owner_cpy = malloc(sizeof(*owner_cpy) * (strlen(owner) + 1));
 	dog_t *another_dog = malloc(sizeof(*another_dog));
+	int i;
 
-	if (!another_dog)
+	if (!another_dog || !name_cpy || !owner_cpy)
 		return (NULL);
 
-	another_dog->name = nm;
+	for (i = 0; name[i]; i++)
+		name_cpy[i] = name[i];
+	name_cpy[i] = '\0';
+
+	for (i = 0; owner[i]; i++)
+		owner_cpy[i] = owner[i];
+	owner_cpy[i] = '\0';
+
+	another_dog->name = name_cpy;
 	another_dog->age = age;
-	another_dog->owner = ownr;
+	another_dog->owner = owner_cpy;
 
 	return (another_dog);
 }
