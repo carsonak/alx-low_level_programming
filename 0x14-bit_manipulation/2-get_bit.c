33d93feecc938596540ecf588e5e2f5d36f89a47
Andrew <49104171+carsonak@users.noreply.github.com>
2023-10-27 05:48:00 +0300

    Fixed logic for 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,44 +9,7 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	char *bi_num;
-	int idx_i = -1;
-	unsigned int i, digits = 0;
-	unsigned long int pow = 2;
+	n >>= index;
 
-	while (pow <= n)
-	{
-		pow *= 2;
-		digits++;
-	}
-
-	if (index > digits)
-		return (idx_i);
-
-	bi_num = malloc(sizeof(*bi_num) * (digits + 1));
-
-	if (bi_num)
-	{
-		idx_i = 0;
-		for (i = 0; n; i++)
-		{
-			if (n & 1)
-				bi_num[i] = '1';
-			else
-				bi_num[i] = '0';
-
-			if (i == index)
-			{
-				idx_i = bi_num[i] - '0';
-				break;
-			}
-
-			n >>= 1;
-		}
-	}
-	else
-		return (idx_i);
-
-	free(bi_num);
-	return (idx_i);
+	return (n & 1);
 }
