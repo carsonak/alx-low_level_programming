7d7e7a0ab9101993f93ecbee2fbbc714a364e340
Andrew <49104171+carsonak@users.noreply.github.com>
2023-11-16 04:03:03 +0300

    revision

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,14 +9,11 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
+	unsigned int i = 0;
 
-	while (s2[i] != '\0' || s1[i] != '\0')
-	{
+	for (i = 0; (s2[i] != '\0') || (s1[i] != '\0'); i++)
 		if (s1[i] != s2[i])
 			return (s1[i] - s2[i]);
-		i++;
-	}
 
 	return (0);
 }
