5aaf5a4fee2332d0c4f655a6689002628ffd4e4a
Andrew <49104171+carsonak@users.noreply.github.com>
2024-02-22 02:22:54 +0300

    Break

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""""""
+"""Module for island_perimeter and it's functions"""
 
 
-def check_grid(grid: list[list[int]]):
+def check_grid(grid):
     """Check types and values of all grid items"""
 
     if type(grid) is not list:
@@ -20,52 +20,52 @@ def check_grid(grid: list[list[int]]):
                 raise ValueError("Value of cell is out of range")
 
 
-def find_next(grid: list[list[int]], x: int, y: int) -> dict[str, int]:
-    """Marks which cells around a given coordinate are filled"""
+def find_next(grid, x, y):
+    """Return a dictionary with info about the cells around a coordinate"""
 
     compass = {"N": 0, "NE": 0, "E": 0, "SE": 0,
                "S": 0, "SW": 0, "W": 0, "NW": 0}
     # Check cells above (Northern)
     if x - 1 >= 0:
         # North
-        if y < len(grid[x - 1]) and grid[x - 1][y]:
+        if y < len(grid[x - 1]) and grid[x - 1][y] == 1:
             compass["N"] = 1
 
         # North-East
-        if y + 1 < len(grid[x - 1]) and grid[x - 1][y + 1]:
+        if y + 1 < len(grid[x - 1]) and grid[x - 1][y + 1] == 1:
             compass["NE"] = 1
 
         # North-West
-        if y - 1 >= 0 and grid[x - 1][y - 1]:
+        if y - 1 >= 0 and grid[x - 1][y - 1] == 1:
             compass["NW"] = 1
 
     # Check cells below (Southern)
     if x + 1 < len(grid):
         # South
-        if y < len(grid[x + 1]) and grid[x + 1][y]:
+        if y < len(grid[x + 1]) and grid[x + 1][y] == 1:
             compass["S"] = 1
 
         # South-East
-        if y + 1 < len(grid[x + 1]) and grid[x + 1][y + 1]:
+        if y + 1 < len(grid[x + 1]) and grid[x + 1][y + 1] == 1:
             compass["SE"] = 1
 
         # South-West
-        if y - 1 >= 0 and grid[x + 1][y - 1]:
+        if y - 1 >= 0 and grid[x + 1][y - 1] == 1:
             compass["SW"] = 1
 
     # Check cell on right (East)
-    if y + 1 < len(grid[x]) and grid[x][y + 1]:
+    if y + 1 < len(grid[x]) and grid[x][y + 1] == 1:
         compass["E"] = 1
 
     # Check cell on the left (West)
-    if y - 1 >= 0 and grid[x][y - 1]:
+    if y - 1 >= 0 and grid[x][y - 1] == 1:
         compass["W"] = 1
 
     return compass
 
 
-def draw_perimeter(grid: list[list[int]], perimeter: list[list[int]]):
-    """"""
+def first_cell(grid, peri):
+    """Mark a counter grid with the peri"""
 
     for x, row in enumerate(grid):
         for y, col in enumerate(row):
@@ -90,27 +90,26 @@ def draw_perimeter(grid: list[list[int]], perimeter: list[list[int]]):
     else:
         key = "N"
 
-    explorer(grid, x, y, perimeter, key)
+    return x, y, key
 
 
-def explorer(grid: list[list[int]], x: int, y: int,
-             perimeter: list[list[int]], dir: str):
-    """Moves in only one direction, will call itself recursively to explore other directions"""
+def explorer(grid, peri, x, y, dir):
+    """Explore the grid in one direction while marking peri"""
 
-    def path_split(path, up, down):
-        """Checks if path is a path"""
+    def path_split(frk, up, down):
+        """Determine if there is a fork toward the given direction"""
 
         way = None
-        if comp[path]:
-            if not comp[path + "E" if path == "N" or path == "S" else "N" + path]\
-                    and not comp[path + "W" if path == "N" or path == "S" else "S" + path]:
-                if path == "N":
+        if comp[frk]:
+            if not (comp[frk + "E" if frk == "N" or frk == "S" else "N" + frk]
+                    and comp[frk + "W" if frk == "N" or frk == "S" else "S" + frk]):
+                if frk == "N":
                     up -= 1
-                elif path == "E":
+                elif frk == "E":
                     down += 1
-                elif path == "S":
+                elif frk == "S":
                     up += 1
-                elif path == "W":
+                elif frk == "W":
                     down -= 1
 
                 way = (up, down)
@@ -118,7 +117,6 @@ def explorer(grid: list[list[int]], x: int, y: int,
         return way
 
     cardinal = ["N", "E", "S", "W"]
-    # cad_8 = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"]
     pop = cardinal.index(dir)
     comp = {dir: 1}
     while comp[dir] and 0 <= x < len(grid) and 0 <= y < len(grid[x]):
@@ -126,23 +124,23 @@ def explorer(grid: list[list[int]], x: int, y: int,
         for point in cardinal:
             if not comp[point]:
                 if point == "N":
-                    perimeter[x - 1][y] += 1
+                    peri[x - 1][y] += 1
                 elif point == "E":
-                    perimeter[x][y + 1] += 1
+                    peri[x][y + 1] += 1
                 elif point == "S":
-                    perimeter[x + 1][y] += 1
+                    peri[x + 1][y] += 1
                 elif point == "W":
-                    perimeter[x][y - 1] += 1
+                    peri[x][y - 1] += 1
 
         change = path_split(cardinal[(pop+1) % 4], x, y)
         if change:
-            explorer(grid, change[0], change[1],
-                     perimeter, cardinal[(pop+1) % 4])
+            explorer(grid, peri, change[0], change[1],
+                     cardinal[(pop+1) % 4])
 
         change = path_split(cardinal[(pop-1) % 4], x, y)
         if change:
-            explorer(grid, change[0], change[1],
-                     perimeter, cardinal[(pop-1) % 4])
+            explorer(grid, peri, change[0], change[1],
+                     cardinal[(pop-1) % 4])
 
         if dir == "N":
             x -= 1
@@ -155,13 +153,15 @@ def explorer(grid: list[list[int]], x: int, y: int,
 
 
 def island_perimeter(grid):
-    """"""
+    """Calculate perimeter of an Island"""
 
     check_grid(grid)
-    peri = [[0 for y in x] for x in grid]
-    draw_perimeter(grid, peri)
+    perimeter = [[0 for y in x] for x in grid]
+    x, y, direction = first_cell(grid, perimeter)
+    explorer(grid[:], perimeter, x, y, direction)
+
     total = 0
-    for row in peri:
+    for row in perimeter:
         for cel in row:
             total += cel
 
