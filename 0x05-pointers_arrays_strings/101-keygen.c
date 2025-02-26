d9196a15af3cce613dafa174cbcf5936e2dfee51
Andrew <49104171+carsonak@users.noreply.github.com>
2025-02-26 14:07:42 +0300

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print a string with a checksum of 2772.
+ *
+ * Return: always 0.
+ */
+int main(void)
+{
+	printf("dddddddddddddddddddddddddddH");
+	return (0);
+}
