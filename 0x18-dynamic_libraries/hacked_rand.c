af5b13276341102368c71dc044e0ea0cc3bc5452
Andrew <49104171+carsonak@users.noreply.github.com>
2025-02-27 13:50:22 +0300

    Fixed order of numbers

--- 0x18-dynamic_libraries/hacked_rand.c
+++ 0x18-dynamic_libraries/hacked_rand.c
@@ -8,7 +8,7 @@ int rand(void)
 	static int i;
 	/* numbers should not repeat */
 	/* wining number = (rand() % 75) + 1 */
-	int wining_numbers[] = {8, 8 + 75, 7, 9, 74, 23};
+	int wining_numbers[] = {8, 8 + 75, 7, 9, 23, 74};
 
 	++i;
 	return (wining_numbers[i - 1]);
